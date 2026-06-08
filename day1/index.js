a8e47379952b6f5c2c6ccab3a0898467276b24d9
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-06-08 23:10:43 +0600

    solve Find the Largest of Three Numbers

--- day1/index.js
+++ day1/index.js
@@ -19,13 +19,31 @@
 // Input: 4  → Output: trueInput: 7  → Output: false
 // Hint: Use the modulus (%) operator.
 
-function isEven(n){
-    if(n % 2 == 0){
-        return true;
-    }else{
-        return false;
+// function isEven(n){
+//     if(n % 2 == 0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+// console.log(isEven(4))
+// console.log(isEven(3))
+// console.log(isEven(99))
+
+// Problem 3: Find the Largest of Three Numbers  [Easy]
+// Description: Write a function largest(a, b, c) that returns the largest of three numbers.
+// Example:
+// Input: 3, 7, 5  → Output: 7
+// Hint: Use Math.max() or if-else conditions.
+ function largest(a, b, c){
+    if(a>b && a>c){
+        return a;
     }
-}
-console.log(isEven(4))
-console.log(isEven(3))
-console.log(isEven(99))
+    else if(b>a && b>c){
+return b;
+    }
+    else{
+        return c;
+    }
+ }
+    console.log(largest(1000,3,2))
