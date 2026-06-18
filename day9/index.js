9ad0d2140df24d86d594357fad599b98ea82c17a
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-06-18 23:21:09 +0600

    solve the equation for 2 element addition

---
+++ day9/index.js
@@ -0,0 +1,20 @@
+// Problem 41: Two Sum  [Easy]
+// Description: Given an array of numbers and a target, write a
+//  function twoSum(arr, target) that returns the indices of the two 
+//  numbers that add up to the target.
+// Example:
+// Input: [2, 7, 11, 15], target = 9Output: [0, 1]  (because 2 + 7 = 9)
+// Hint: Use a hash map to store visited values and their indices.
+function twoSum(arr, target){
+    const map={}
+    for(let i=0;i<arr.length;i++){
+        const complement=target-arr[i]
+        if(map[complement]!==undefined){
+            return [map[complement],i]
+        }        map[arr[i]]=i
+    }
+
+}
+const arr=[2,7,11,15]
+const target=13
+console.log(twoSum(arr,target)) 
