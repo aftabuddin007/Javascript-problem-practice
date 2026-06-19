35498d3a89ba89f147a4a16f6c6c4a511634fbd0
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-06-19 20:16:02 +0600

    solve  Binary Search

--- day9/index.js
+++ day9/index.js
@@ -53,19 +53,46 @@
 // Example:
 // Input: '()[]{}'  → Output: trueInput: '([)]'    → Output: false
 // Hint: Use a stack (array). Push opening brackets, pop and compare for closing ones.
-function isValidParentheses(str){
-    const stack = [];
-    const pairs = { ')': '(', ']': '[', '}': '{' };
-    for (let char of str) {
-        if (['(', '[', '{'].includes(char)) {
-            stack.push(char);
-        } else if ([')', ']', '}'].includes(char)) {
-            if (stack.pop() !== pairs[char]) {
-                return false;
-            }
+// function isValidParentheses(str){
+//     const stack = [];
+//     const pairs = { ')': '(', ']': '[', '}': '{' };
+//     for (let char of str) {
+//         if (['(', '[', '{'].includes(char)) {
+//             stack.push(char);
+//         } else if ([')', ']', '}'].includes(char)) {
+//             if (stack.pop() !== pairs[char]) {
+//                 return false;
+//             }
+//         }
+//     }
+//     return stack.length === 0;
+
+// }
+// console.log(isValidParentheses("()[)}[]"))
+ 
+// Problem 45: Binary Search  [Medium]
+// Description: Write a function binarySearch(arr, target) 
+// that searches a sorted array and returns the index of the target, or -1 if not found.
+// Example:
+// Input: [1,3,5,7,9], target=7  → Output: 3
+// Hint: Use left and right pointers; check the middle element each iteration.
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
-    return stack.length === 0;
+    return -1;
 
 }
-console.log(isValidParentheses("()[)}[]"))
+const arr = [1, 3, 5, 7, 9];
+const target = 56;
+console.log(binarySearch(arr, target))
+
