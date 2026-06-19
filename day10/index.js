ebb31ced1329c238324a3d5ff1c03c5a178913a8
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-06-20 00:57:12 +0600

    solve  Roman to Integer

--- day10/index.js
+++ day10/index.js
@@ -46,10 +46,37 @@
 // Example:
 // Input: [1,2,3,4,5], k=2Output: [4,5,1,2,3]
 // Hint: Use slice and concat, or reverse the whole array then parts.
-function rotateArray(arr, k){
-    const n = arr.length;
-    k = k % n;
-    return arr.slice(-k).concat(arr.slice(0, n - k));
+// function rotateArray(arr, k){
+//     const n = arr.length;
+//     k = k % n;
+//     return arr.slice(-k).concat(arr.slice(0, n - k));
+
+// }
+// console.log(rotateArray([1,2,3,4,5], 2));
+// Problem 49: Roman to Integer  [Medium]
+// Description: Write a function romanToInt(s)
+//  that converts a Roman numeral string to an integer.
+// Example:
+// Input: 'III'   → Output: 3Input: 'IX'    → Output: 9Input: 'LVIII' → Output: 58
+// Hint: Map each symbol to its value; if 
+// a smaller value comes before a larger one, subtract it.
+
+function romanToInt(s){
+    const romanMap = {
+    'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
+        const currentVal = romanMap[s[i]];
+        const nextVal = romanMap[s[i + 1]];
+        if (nextVal && currentVal < nextVal) {
+            total -= currentVal;
+        }else {
+            total += currentVal;
+        }
+    }
+    return total;
+    
+
 
 }
-console.log(rotateArray([1,2,3,4,5], 2));
+console.log(romanToInt("LVIII"));
