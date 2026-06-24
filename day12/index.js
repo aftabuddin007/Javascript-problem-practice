296c257d2adeda6a60b754e76594cc9d88b18c5e
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-06-24 17:39:16 +0600

    Minimum Difference Between Highest and Lowest of K Scores

--- day12/index.js
+++ day12/index.js
@@ -48,24 +48,51 @@
 // Input: 16 → Output: true | Input: 14 → Output: false
 // Hint: You can use Binary Search! Set left = 1 and right = n. Find the
 //  middle element, calculate mid * mid, and compare it with n. Repeat until found or pointers cross.
-function isPerfectSquare(n){
-    if (n < 2) return true;
-    let left = 1; 
-    right = n;    
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
-        const square = mid * mid;
-        if (square === n) 
-            return true;
-        if (square < n) {
-            left = mid + 1;
-        }
-        else {
-            right = mid - 1;
-        }
+// function isPerfectSquare(n){
+//     if (n < 2) return true;
+//     let left = 1; 
+//     right = n;    
+//     while (left <= right) {
+//         const mid = Math.floor((left + right) / 2);
+//         const square = mid * mid;
+//         if (square === n) 
+//             return true;
+//         if (square < n) {
+//             left = mid + 1;
+//         }
+//         else {
+//             right = mid - 1;
+//         }
+//     }
+//     return false;
+
+// }
+// const input = 66;
+// console.log(isPerfectSquare(input))
+
+
+// Problem 59: Minimum Difference Between Highest and Lowest of K Scores  [Easy]
+// Description: Given a 0-indexed integer array nums, where nums[i] represents the score of the ith student, 
+//  and an integer k. Write a function minimumDifference(nums, k) that returns the minimum possible difference 
+//  between the highest and lowest score among any choice of k students.
+// Example:
+// Input: nums = [9, 4, 1, 7], k = 2 → Output: 2 
+//  (If we choose scores [9, 7], the difference is 9 - 7 = 2, which is the minimum possible difference)
+// Hint: First, sort the array in ascending order. Then, use a sliding window of size k. The difference for 
+// any window starting at index i will be nums[i + k - 1] - nums[i]. Find the minimum of all such differences.
+function minimumDifference(nums, k){
+    const n = nums.length;
+    if (k <= 1 || n < k) return 0; 
+    nums.sort((a, b) => a - b); 
+    let minDiff = Infinity;
+
+    for (let i = 0; i <= n - k; i++) {
+        const diff = nums[i + k - 1] - nums[i];
+        minDiff = Math.min(minDiff, diff);
     }
-    return false;
+    return minDiff;
 
 }
-const input = 66;
-console.log(isPerfectSquare(input))
+const nums = [9, 4, 1, 7];
+const k = 3;
+console.log(minimumDifference(nums, k));
